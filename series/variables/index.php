49e8e8d065a1ed52fc3c7217f0e59c94b1fcb0fb
Jose <36056739+Yepez719@users.noreply.github.com>
2018-09-07 21:06:27 -0400

    Add files via upload
    Went through the different types of variables and how they are declared

---
+++ series/variables/index.php
@@ -0,0 +1,18 @@
+
+
+<?php
+/* Every variable starts with a $ in PHP. Declare variable at the
+top and call it below. The words "string" and "number" are not
+neccesary to declare the variable */
+
+$text = 'Hello World.';
+$number = 100;
+
+echo $text;
+echo $number;
+
+
+
+
+
+ ?>
