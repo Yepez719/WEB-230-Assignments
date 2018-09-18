3d40e21f59af9795c700b88f0caf634ac96eab26
Jose <36056739+Yepez719@users.noreply.github.com>
2018-09-18 16:22:13 -0400

    Add files via upload

---
+++ series/switch/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$day = 'Saturday';
+
+switch ($day){
+
+     case 'Saturday':
+     case 'Sunday':
+          echo 'It\'s a weekend.';
+     break;
+
+     default:
+          echo 'Not a weekend.';
+          break;
+
+
+
+
+
+}
+
+ ?>
