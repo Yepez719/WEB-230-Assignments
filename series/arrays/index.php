0303178c9784839c4362ea5da4e16f79e305092f
Jose <36056739+Yepez719@users.noreply.github.com>
2018-10-18 19:50:10 -0400

    Add files via upload

---
+++ series/arrays/index.php
@@ -0,0 +1,12 @@
+<?php
+//Arrays start at 0//
+$food = array('Pasta', 'Pizza', 'Salad', 'Vegetable');
+
+//This method will become more useful later. Good for loops
+$food[4]='Fruit';
+
+//Using print instead of echo shows the details
+print_r($food);
+
+
+?>
